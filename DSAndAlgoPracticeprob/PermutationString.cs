7c4f07547becbabeedac179bc8568531313ea0c7
nibedita260 <deepi.nibedita260.com>
2021-10-05 17:38:15 +0530

    [Nibedita]:Added PermutationString Algorithm Problem Features Implemented

---
+++ DSAndAlgoPracticeprob/PermutationString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSAndAlgoPracticeprob
+{
+    class PermutationString
+    {
+        public void PrintPermutations(string str,int startindex,int endindex)
+        {
+                if (startindex==endindex)
+                {
+                    Console.WriteLine("Permutation of given string is:" + str);
+                }
+                else
+                {
+                    for (int j = startindex; j <= endindex; j++)
+                    {
+                        str = Swap(str, startindex, j);
+                        PrintPermutations(str, startindex + 1, endindex);
+                        str = Swap(str, startindex, j);
+                    }
+                }
+        }
+        public static String Swap(string s,int i,int j)
+        {
+            char temp;
+            char[] charArr = s.ToCharArray();
+            temp = charArr[i];
+            charArr[i] = charArr[j];
+            charArr[j] = temp;
+            string res = new string(charArr);
+            return res;
+        }
+    }
+}
---
+++ DSAndAlgoPracticeprob/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DSAndAlgoPracticeprob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int options;
+            bool isExit = false;
+            while (!isExit)
+            {
+                Console.WriteLine("choose 1.PermutationOfString ");
+                options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        Console.WriteLine("enter a string");
+                        string str = Console.ReadLine();
+                        int n = str.Length;
+                        PermutationString permutationString = new PermutationString();
+                        permutationString.PrintPermutations(str,0,n-1);
+                        break;
+                    default:Console.WriteLine("choose valid option");
+                        break;
+                }
+            }
+        }
+    }
+}
