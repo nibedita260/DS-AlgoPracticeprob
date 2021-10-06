6c8b970b36760d4e84b2ba571325aff56d19e54e
nibedita260 <deepi.nibedita260.com>
2021-10-06 13:46:40 +0530

    [Nibedita]:Added Algorithm problem Features Implemented

--- DSAndAlgoPracticeprob/PalindromePrime.cs
+++ DSAndAlgoPracticeprob/PalindromePrime.cs
@@ -9,57 +9,6 @@ namespace DSAndAlgoPracticeprob
         public static List<int> list = new List<int>();
         public void CheckIsPrimePalindromeAnagrams()
         {
-<<<<<<< HEAD
-            int n = 400;
-            for (int i = 2; i <= n; i++)
-            {
-                if (n % i != 0 && i%2!=0)
-                {
-                    list.Add(i);
-                }
-            }
-            foreach (var elem in list)
-            {
-                if (ISCheckAnagrams(elem) == true && ISCheckPalindrome(elem) == true)
-                {
-                    Console.WriteLine(elem + " is prime number ,palindrome and andanagrams as well");
-                }
-                else {
-                    Console.WriteLine(elem);
-                } 
-            }
-        }
-        public bool ISCheckPalindrome(int num)
-        {
-            int revNum = 0;
-            while (num != 0)
-            {
-                int digit;
-                digit = num % 10;
-                num = num / 10;
-                revNum = revNum * 10 + digit;
-            }
-            if (num == revNum)
-            {
-                return true;
-            }
-            return false;
-        }
-        public bool ISCheckAnagrams(int num)
-        {
-            string str = num.ToString();
-            char[] charArr = str.ToCharArray();
-            Array.Sort(charArr);
-            string res = new string(charArr);
-            foreach(var n in list)
-            {
-                string s = n.ToString();
-                char[] charAr = s.ToCharArray();
-                Array.Sort(charAr);
-                string resStr = new string(charAr);
-                if (resStr == res)
-                {
-=======
             int a = 1, b = 30, i, j, flag;
             Console.WriteLine("\nPrime numbers between " +
                              "{0} and {1} are: ", a, b);
@@ -79,14 +28,14 @@ namespace DSAndAlgoPracticeprob
                 }
                 if (flag == 1)
                 {
-                   // Console.Write(i + " ");
+                    // Console.Write(i + " ");
                     list.Add(i);
                 }
 
             }
             foreach (var elem in list)
             {
-                if (ISCheckPalindrome(elem)==true && ISCheckAnagrams(elem)==true)
+                if (ISCheckPalindrome(elem) == true && ISCheckAnagrams(elem) == true)
                 {
                     Console.WriteLine(elem + " is prime number ,palindrome and andanagrams as well");
                 }
@@ -123,7 +72,6 @@ namespace DSAndAlgoPracticeprob
                 string resStr = new string(charAr);
                 if (resStr == res)
                 {
->>>>>>> UC5-PalindromePrime
                     return true;
                 }
             }
--- DSAndAlgoPracticeprob/PrimeNumbers.cs
+++ DSAndAlgoPracticeprob/PrimeNumbers.cs
@@ -8,17 +8,6 @@ namespace DSAndAlgoPracticeprob
     {
         public void PrintPrimeNumbers()
         {
-<<<<<<< HEAD
-            int n = 8;
-            int flag = 0;
-            for (int i = 2; i <= n; i++)
-            {
-                if (n % i == 0 && i % 2 == 0)
-                {
-                    Console.WriteLine("Number is not Prime.");
-                    flag = 1;
-                    break;
-=======
             int a = 1, b = 100, i, j, flag;
             Console.WriteLine("\nPrime numbers between " +
                               "{0} and {1} are: ", a, b);
@@ -39,12 +28,9 @@ namespace DSAndAlgoPracticeprob
                 if (flag == 1)
                 {
                     Console.Write(i + " ");
->>>>>>> UC5-PalindromePrime
                 }
 
             }
-            if (flag == 0)
-                Console.WriteLine("Number is Prime.");
         }
     }
 }
